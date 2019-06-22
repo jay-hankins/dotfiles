5dbc444886fa08435eb8f692349b36b6d9d83921
Jay Hankins <hankins.jay@gmail.com>
2019-06-22 07:54:41 -0700

    Use GPG

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,3 +3,8 @@ test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shel
 
 # Added by Krypton
 set -x GPG_TTY (tty)
+
+# Added by jay
+export SSH_AUTH_SOCK=(gpgconf --list-dirs agent-ssh-socket)
+gpgconf --launch gpg-agent
+
